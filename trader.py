9fa4d95948dbdbcaac8b8eacf29f902f79e10677
Eryk Halicki <35389308+Stargor14@users.noreply.github.com>
2021-02-05 11:44:22 -0500

    Add files via upload

---
+++ trader.py
@@ -0,0 +1,7 @@
+'''
+maybe make a recomendaion engine? that u can approve the trades of, like ui and can select differnt indicators and use stop losses, trailing stops etc.
+
+historical data? coindesk api, figure out format
+calculate % change from entry price to take profit
+
+'''
