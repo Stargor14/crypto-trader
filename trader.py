7003abb448179293abc438f16c442d1514d20e79
Eryk Halicki <35389308+Stargor14@users.noreply.github.com>
2021-02-08 22:59:25 -0500

    Add files via upload

--- trader.py
+++ trader.py
@@ -4,4 +4,12 @@ maybe make a recomendaion engine? that u can approve the trades of, like ui and 
 historical data? coindesk api, figure out format
 calculate % change from entry price to take profit
 
+call price every tick, tick length adjustable?
+RSI, MACD, Volatility, MA's
 '''
+from binance.client import Client
+import req 
+
+apikey = "x15G8QtfrUCJG1F7tCahCwyCwxE7a3Mbykg8Q4Uf0Q7QKjB1B3GvCYkfzRUTS96e"
+secretkey = "YyATCoS7OwFFAdaqK3UCw0zZLZoz6RWRqarMrHhGi7P08c7Muay8zDWZfV86SxA5"
+
