89651c8979db2ba471cfe54d14f3627e33d396e0
stargor14 <stargor14@outlook.com>
2021-02-10 08:20:53 -0500

    wtf is this tetx?

--- trader.py
+++ trader.py
@@ -6,8 +6,11 @@ from binance.client import Client
 apikey = "x15G8QtfrUCJG1F7tCahCwyCwxE7a3Mbykg8Q4Uf0Q7QKjB1B3GvCYkfzRUTS96e"
 secretkey = "YyATCoS7OwFFAdaqK3UCw0zZLZoz6RWRqarMrHhGi7P08c7Muay8zDWZfV86SxA5"
 
+client = Client(apikey, secretkey)
+
 def openOrder():
     print("shorted!")
 def check(rsi):
+    print(client.get_exchange_info())
     if rsi>=70:
         openOrder()
