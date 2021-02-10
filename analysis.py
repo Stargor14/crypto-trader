c524960565f840b9ec7687ac6dfd403a0031aa45
stargor14 <stargor14@outlook.com>
2021-02-10 15:35:13 -0500

    rsi changes, wip

--- analysis.py
+++ analysis.py
@@ -8,19 +8,29 @@ def loop():
     run()
 
 def run():
-    posp = 0
-    negp = 0
-    for i in range(rsilength):
-        if ((req.prices[i][2]/req.prices[i][1])-1)>0:
-            posp += ((req.prices[i][2]/req.prices[i][1])-1)*100
-        if ((req.prices[i][2]/req.prices[i][1])-1)<0:
-            negp += ((req.prices[i][2]/req.prices[i][1])-1)*100
-    #declare averadge gains/losses
-    posa = posp/rsilength
-    nega = negp/rsilength
-    #step 2 rsi
-    rsi = 100 - (100/(1+(((posa*(rsilength-1))+(req.prices[0][2]/req.prices[0][1]))/((nega*(rsilength-1))+(req.prices[0][2]/req.prices[0][1])))))
-    print(rsi)
+    poss = 0
+    negs = 0
+    for i in range(1,rsilength):
+        if ((req.prices[i][2]/req.prices[i][1])-1)*100>=0:
+            poss += (((req.prices[i][2]/req.prices[i][1])-1)*100)
+        if ((req.prices[i][2]/req.prices[i][1])-1)*100<=0:
+            negs += (((req.prices[i][2]/req.prices[i][1])-1)*100)
+    nega = negs/rsilength
+    posa = poss/rsilength
+    currdiff = ((req.prices[0][2]/req.prices[0][1])-1)*100
+    if currdiff>=0:
+        rsi = 100 -(100/(1+((posa+currdiff)/((-1)*(nega)))))
+    if currdiff<=0:
+        rsi = 100 -(100/(1+((posa)/((-1)*(nega+currdiff)))))
+    av = 0
+    for i in range(5):
+        av+=req.prices[i][2]
+    av = av/5
+    if av>req.prices[0][2]:
+        print(av,req.prices[0][2],"lower", rsi)
+    if av<req.prices[0][2]:
+        print(av,req.prices[0][2],"higher", rsi)
+    #print(f"RSI: {rsi} Price: {req.prices[0][2]} Price diff this candle: {((req.prices[0][2]/req.prices[0][1])-1)*100}%")
     trader.check(rsi)
     req.run(tf)
     loop()
