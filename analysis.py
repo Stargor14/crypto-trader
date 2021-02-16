9b528c660a150755bdc8ca861584af226488241c
stargor <stargor14@outlook.com>
2021-02-16 11:12:35 -0500

    recording

--- analysis.py
+++ analysis.py
@@ -39,19 +39,22 @@ def run(prices,rsi,macd,signal,row):
     diff = prices[row]['close']-prices[row]['open'] #current difference
 
     if inTrade == False: #open conditions go here
-        if rsi>=70:
+        if rsi>=70 and row>broker.cooldown:
             primeds = True
             primedl = False
-        if primedl == True and signal<macd:
+            '''
+        if primedl == True and signal<macd and rsi>30 and row>broker.cooldown:
             entry = prices[row]['close']
             broker.long(entry,row)
             inTrade = True
             inLong = True
             primedl = False
-        if rsi<=30:
+            '''
+        if rsi<=30 and row>broker.cooldown:
             primedl = True
             primeds = False
-        if primeds == True and signal>macd:
+
+        if primedl == True and signal>macd and rsi<70 and row>broker.cooldown:
             entry = prices[row]['close']
             broker.short(entry,row)
             inTrade = True
@@ -60,6 +63,7 @@ def run(prices,rsi,macd,signal,row):
         #if rsi>45 and rsi<55:
             #primedl = False
             #primeds = False
+
     if inTrade == True: #close conditions go here
         if inLong == True:
             exitl = prices[row]['low']
@@ -67,11 +71,11 @@ def run(prices,rsi,macd,signal,row):
             pNll = (exitl/entry-1)*100
             pNlh = (exith/entry-1)*100
             if pNlh>=takeProfit or signal>macd and pNlh>=takeProfit/4 or rsi>70  and pNlh>=takeProfit/4:
-                broker.close(exith,pNlh,row)
+                broker.close(exith,pNlh,row,'l')
                 inTrade = False
                 inLong = False
             if pNll<=stopLoss:
-                broker.close(exitl,stopLoss,row)
+                broker.close(exitl,stopLoss,row,'l')
                 inTrade = False
                 inLong = False
         if inShort == True:
@@ -80,10 +84,10 @@ def run(prices,rsi,macd,signal,row):
             pNll = -(exitl/entry-1)*100
             pNlh = -(exith/entry-1)*100
             if pNll>=takeProfit or signal<macd and pNll>=takeProfit/4 or rsi<30  and pNll>=takeProfit/4:
-                broker.close(exitl,pNll,row)
+                broker.close(exitl,pNll,row,'s')
                 inTrade = False
                 inShort = False
             if pNlh<=stopLoss:
-                broker.close(exith,stopLoss,row)
+                broker.close(exith,stopLoss,row,'s')
                 inTrade = False
                 inShort = False
--- broker.py
+++ broker.py
@@ -18,6 +18,16 @@ global balance
 balance = int(input("Starting balance: "))
 global trades
 trades=[]
+global cooldown
+cooldown = 0
+global goodlong
+goodlong =0
+global goodshort
+goodshort=0
+global badlong
+badlong =0
+global badshort
+badshort=0
 def long(en,row):
     print(f"LONG at {en}")
     #long function
@@ -26,13 +36,29 @@ def short(en,row):
     print(f"SHORT at {en}")
     #short function
     trades.append([row,en,'s'])
-def close(ex,pnl,row):
+def close(ex,pnl,row,type):
     global balance
     global profit
     global tradesum
+    global cooldown
+    global goodlong
+    global goodshort
+    global badshort
+    global badlong
     tradesum +=1
     balance=((pnl-.08)/100+1)*balance
     profit+=pnl-.08
     trades.append([row,ex,'c'])
     print(f"CLOSED at {ex} mith pNl of: {pnl}")
-    print(f"Total balance: {balance} Total profit: {profit}% Total trades: {tradesum}")
+    print(f"Total balance: {balance} Total profit: {profit}% Total trades: {tradesum} Good longs: {goodlong} Good shorts: {goodshort} Bad longs: {badlong} Bad shorts: {badshort}")
+    if pnl <0:
+        cooldown = row+60
+        if type == 'l':
+            badlong+=1
+        else:
+            badshort+=1
+    if pnl>0:
+        if type == 'l':
+            goodlong+=1
+        else:
+            goodshort+=1
