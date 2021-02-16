e54a4d55ac606522eaaaa5641edea767c8368a80
stargor <stargor14@outlook.com>
2021-02-16 11:37:46 -0500

    updates

--- technicals.py
+++ technicals.py
@@ -26,7 +26,7 @@ def macd():
     signal = macd.ewm(span=9, adjust=False).mean()
     return macd,signal
 
-def test(prices,rsia,macda,signala,takeProfit,stopLoss):
+def test(prices,rsia,macda,signala,takeProfits,stopLosss,takeProfitl,stopLossl,):
     primeds = False
     primedl = False
     inTrade = False
@@ -50,11 +50,13 @@ def test(prices,rsia,macda,signala,takeProfit,stopLoss):
             if rsia[i]<=rsimin:
                 primedl = True
                 primeds = False
+
             if primeds == True and signala[i]>macda[i] and rsia[i]<rsimax:
             #if primeds == True and rsia[i]<rsimax:
                 inTrade = True
                 inShort = True
                 primeds = False
+
             #if ((rsimax+rsimin)/2)-((rsimax+rsimin)/20)<rsi<((rsimax+rsimin)/2)+((rsimax+rsimin)/20):
                 #primedl = False
                 #primeds = False
@@ -64,13 +66,13 @@ def test(prices,rsia,macda,signala,takeProfit,stopLoss):
                 exith = prices[i]['high']
                 pNll = (exitl/entry-1)*100
                 pNlh = (exith/entry-1)*100
-                if pNlh>=takeProfit or signala[i]>macda[i] and pNlh>=takeProfit/2 or rsia[i]>rsimax  and pNlh>=takeProfit/2:
+                if pNlh>=takeProfitl or signala[i]>macda[i] and pNlh>=takeProfitl/2 or rsia[i]>rsimax  and pNlh>=takeProfitl/2:
                     balance=balance*(((pNlh-.08)/100)+1)
                     trades+=1
                     inTrade = False
                     inLong = False
-                if pNll<=stopLoss:
-                    balance=balance*(((stopLoss-.08)/100)+1)
+                if pNll<=stopLossl:
+                    balance=balance*(((stopLossl-.08)/100)+1)
                     trades+=1
                     inTrade = False
                     inLong = False
@@ -80,13 +82,13 @@ def test(prices,rsia,macda,signala,takeProfit,stopLoss):
                 exitc = prices[i]['close']
                 pNll = -(exitl/entry-1)*100
                 pNlh = -(exith/entry-1)*100
-                if pNll>=takeProfit or signala[i]<macda[i] and pNll>=takeProfit/2 or rsia[i]<rsimin  and pNll>=takeProfit/2:
+                if pNll>=takeProfits or signala[i]<macda[i] and pNll>=takeProfits/2 or rsia[i]<rsimin  and pNll>=takeProfits/2:
                     balance=balance*(((pNll-.08)/100)+1)
                     trades+=1
                     inTrade = False
                     inShort = False
-                if pNlh<=stopLoss :
-                    balance=balance*(((stopLoss-.08)/100)+1)
+                if pNlh<=stopLosss :
+                    balance=balance*(((stopLosss-.08)/100)+1)
                     trades+=1
                     inTrade = False
                     inShort = False
@@ -99,30 +101,33 @@ def run():
     macda = macd()[0]
     signala = macd()[1]
     prices = req.prices
+    rsia = rsi
     num = 0
-    runs = 12*20*20
-    dataset=[{"RSI LENGTH":0,"TAKE PROFIT":0,"STOP LOSS":0,"BALANCE":0,"TRADES":0}]
-    for rsilength in range(8,20):
-        for takeProfit in range(10,30):
-            for stopLoss in range(-30,-10):
-                if num%10==0 and num>0:
-                    print(f"{num}/{runs} Expected time remaining: {(runs-num)*runtime} seconds")
-                tic = time.perf_counter()
-                balance = test(prices,rsi(rsilength),macda,signala,takeProfit/10,stopLoss/10)
-                if balance[0]>=dataset[0]['BALANCE']:
-                    dataset.append({"RSI LENGTH":rsilength,"TAKE PROFIT":takeProfit,"STOP LOSS":stopLoss,"BALANCE":balance[0],"TRADES":balance[1]})
-                    dataset.sort(key=sorter)
-                    if len(dataset)>50:
-                        dataset.pop(0)
-                toc = time.perf_counter()
-                runtime = round(toc-tic,1)
-                num+=1
+    runs = 5*12*15
+    rsilength = 14
+    dataset=[{"RSI LENGTH":0,"TAKE PROFITL":0,"STOP LOSSL":0,"TAKE PROFITS":0,"STOP LOSSS":0,"BALANCE":0,"TRADES":0}]
+    takeProfitl = 29
+    stopLossl = -23
+    for takeProfits in range(5,30):
+        for stopLosss in range(-30,-5):
+            if num%10==0 and num>0:
+                print(f"{num}/{runs} Expected time remaining: {(runs-num)*runtime} seconds")
+            tic = time.perf_counter()
+            balance = test(prices,rsi(rsilength),macda,signala,takeProfits/10,stopLosss/10,takeProfitl/10,stopLossl/10)
+            if balance[0]>=dataset[0]['BALANCE']:
+                dataset.append({"RSI LENGTH":rsilength,"TAKE PROFITL":takeProfitl,"STOP LOSSL":stopLossl,"TAKE PROFITS":takeProfits,"STOP LOSSS":stopLosss,"BALANCE":balance[0],"TRADES":balance[1]})
+                dataset.sort(key=sorter)
+                if len(dataset)>50:
+                    dataset.pop(0)
+            toc = time.perf_counter()
+            runtime = round(toc-tic,1)
+            num+=1
     #print("Michael is obese and we need to code thge email thingy rn ASAP Rocky type beat")
     with open("Z:\github\data.json", "w") as write_file:
         json.dump(dataset, write_file)
     j=50
     for i in dataset:
-        print(f"rank: {j} RSI length: {i['RSI LENGTH']} Take Profit: {i['TAKE PROFIT']/10} Stop Loss: {i['STOP LOSS']/10} Balance: {round(i['BALANCE'],4)} Trades: {i['TRADES']}")
+        print(f"rank: {j} RSI length: {i['RSI LENGTH']} Take Profit Long: {i['TAKE PROFITL']/10} Stop Loss Long: {i['STOP LOSSL']/10} Take Profit Short: {i['TAKE PROFITS']/10} Stop Loss Short: {i['STOP LOSSS']/10} Balance: {round(i['BALANCE'],2)} Trades: {i['TRADES']}")
         j-=1
 req.run()
 run()
