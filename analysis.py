5f443bdf8b2d861e42601eefd6c31de16733ceec
stargor <stargor14@outlook.com>
2021-02-16 21:19:10 -0500

    updated

--- analysis.py
+++ analysis.py
@@ -34,60 +34,46 @@ def run(prices,rsi,macd,signal,row):
     global exit
     global primeds
     global primedl
-    diff2 = prices[row+2]['close']-prices[row+2]['open']
-    diff1 = prices[row+1]['close']-prices[row+1]['open'] #price diff whole, check if pos or neg
-    diff = prices[row]['close']-prices[row]['open'] #current difference
+    rsimax = 70
+    rsimin =30
 
-    if inTrade == False: #open conditions go here
-        if rsi>=70 and row>broker.cooldown:
+    #enter conditions
+    if inShort == False:
+        if rsi[row]>=rsimax:
             primeds = True
-            primedl = False
-            '''
-        if primedl == True and signal<macd and rsi>30 and row>broker.cooldown:
-            entry = prices[row]['close']
-            broker.long(entry,row)
-            inTrade = True
-            inLong = True
-            primedl = False
-            '''
-        if rsi<=30 and row>broker.cooldown:
-            primedl = True
-            primeds = False
-
-        if primedl == True and signal>macd and rsi<70 and row>broker.cooldown:
-            entry = prices[row]['close']
-            broker.short(entry,row)
-            inTrade = True
+        if primeds == True and signal>macd and rsi<=rsimax:
+            entrys = prices[row]['close']
             inShort = True
             primeds = False
-        #if rsi>45 and rsi<55:
-            #primedl = False
-            #primeds = False
-
-    if inTrade == True: #close conditions go here
-        if inLong == True:
-            exitl = prices[row]['low']
-            exith = prices[row]['high']
-            pNll = (exitl/entry-1)*100
-            pNlh = (exith/entry-1)*100
-            if pNlh>=takeProfit or signal>macd and pNlh>=takeProfit/4 or rsi>70  and pNlh>=takeProfit/4:
-                broker.close(exith,pNlh,row,'l')
-                inTrade = False
-                inLong = False
-            if pNll<=stopLoss:
-                broker.close(exitl,stopLoss,row,'l')
-                inTrade = False
-                inLong = False
-        if inShort == True:
-            exitl = prices[row]['low']
-            exith = prices[row]['high']
-            pNll = -(exitl/entry-1)*100
-            pNlh = -(exith/entry-1)*100
-            if pNll>=takeProfit or signal<macd and pNll>=takeProfit/4 or rsi<30  and pNll>=takeProfit/4:
-                broker.close(exitl,pNll,row,'s')
-                inTrade = False
-                inShort = False
-            if pNlh<=stopLoss:
-                broker.close(exith,stopLoss,row,'s')
-                inTrade = False
-                inShort = False
+    if inLong == False:
+        if rsi<=rsimin:
+            primedl = True
+        if primedl == True and signal<macd and rsi>=rsimin:
+            entryl = prices[row]['close']
+            inLong = True
+            primedl = False
+    if 45<rsi<55:
+        primedl = False
+        primeds = False
+        #close conditions
+    exitl = prices[row]['low']
+    exith = prices[row]['high']
+    exitc = prices[row]['close']
+    if inLong == True:
+        pNll = (exitl/entryl-1)*100
+        pNlh = (exith/entryl-1)*100
+        if pNlh>=takeProfitl or signal>macd and pNlh>=takeProfitl/4 or rsi>rsimax  and pNlh>=takeProfitl/4:
+            broker.close(exith,pNlh,row,'l')
+            inLong = False
+        if pNll<=stopLossl:
+            broker.close(exitl,stopLossl,row,'l')
+            inLong = False
+    if inShort == True:
+        pNll = -(exitl/entrys-1)*100
+        pNlh = -(exith/entrys-1)*100
+        if pNll>=takeProfits or signal<macd and pNll>=takeProfits/4 or rsi<rsimin  and pNll>=takeProfits/4:
+            broker.close(exitl,pNll,row,'s')
+            inShort = False
+        if pNlh<=stopLosss :
+            broker.close(exith,stopLosss,row,'s')
+            inShort = False
