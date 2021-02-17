a023b04eba07e6d2577dda3eac7db9524fefc52e
Andy Stroe <andystroe101@gmail.com>
2021-02-17 11:44:59 -0500

    Beluga whale type shit

--- Demo.py
+++ Demo.py
@@ -6,13 +6,17 @@ def x():
     Email_Adress = 'andy.btc.profit@gmail.com'
     Email_Password = 'sgijlrmqtkskkabi'
 
+    x=100**2
+    if x>=10000:
+        y = 'Michael is now a beluga whale'
     msg = EmailMessage()
-    msg['Subject'] = 'Michaels New Weight!!'
+    msg['Subject'] = 'Hello'
     msg['From'] = Email_Adress
     msg['to'] = 'andy.btc.profit@gmail.com'
-    msg.set_content("x")
+    msg.set_content(f"{y}")
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(Email_Adress, Email_Password)
         smtp.send_message(msg)
+
 x()
