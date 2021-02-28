e88fe64c7ebb5e10b4d9c0eb956fdf94fab990b3
Eryk Halicki <stargor14@outlook.com>
2021-02-28 13:42:02 -0500

    push

---
+++ brain.py
@@ -0,0 +1,12 @@
+'''
+
+                            analysis.py              Current
+                                 |                  /
+        live -- broker.py -- brain.py -- requests.py
+                    |            |                  \
+                   past    technicals.py             Past
+                          /             \
+                          live          past
+
+Within brain => press 1 or 2, 1 for live/paper 2 for backtest
+'''
