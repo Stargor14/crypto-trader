1873ad4015c10ceed581158621d313499dcaae31
Eryk Halicki <stargor14@outlook.com>
2021-02-28 13:14:01 -0500

    wipe

--- exLiveBroker.py
+++ exLiveBroker.py
@@ -1,24 +1,6 @@
-import requests
 import json
-import smtplib
-import os
 from email.message import EmailMessage
 from binance.client import Client
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = 'Z:\github\skeys.json'
-SPREADSHEET_ID = '1z0_KbA4kywx0P6K08PaK0oCaeRkpts1RO-si7BpFUvs'
-
-credentials = None
-credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-service = build('sheets', 'v4', credentials=credentials)
-global sheet
-sheet = service.spreadsheets()
 
 with open('Z:\github\keys.json') as f:
   data = json.load(f)
@@ -31,30 +13,6 @@ global Email_Adress
 Email_Adress = 'andy.btc.profit@gmail.com'
 global Email_Password
 Email_Password = 'sgijlrmqtkskkabi'
-
-def record(prices,rsi,type,pnl):
-    with open('row.json','r') as f:
-      data = json.load(f)
-    row = data['row']
-    timeL = [[prices[0]['time']]]
-    Jtime = {"values":timeL}
-    rsiL = [[round(rsi,2)]]
-    Jrsi = {"values":rsiL}
-    closeL = [[prices[0]['close']]]
-    Jclose = {"values":closeL}
-    typeL = [[type]]
-    Jtype = {"values":typeL}
-    pnlL = [[pnl]]
-    Jpnl = {"values":pnlL}
-    sheet.values().update(spreadsheetId=SPREADSHEET_ID,range=f'A{row}',valueInputOption='USER_ENTERED',body=Jtime).execute()
-    sheet.values().update(spreadsheetId=SPREADSHEET_ID,range=f'B{row}',valueInputOption='USER_ENTERED',body=Jrsi).execute()
-    sheet.values().update(spreadsheetId=SPREADSHEET_ID,range=f'C{row}',valueInputOption='USER_ENTERED',body=Jclose).execute()
-    sheet.values().update(spreadsheetId=SPREADSHEET_ID,range=f'D{row}',valueInputOption='USER_ENTERED',body=Jtype).execute()
-    if type == "close":
-        sheet.values().update(spreadsheetId=SPREADSHEET_ID,range=f'E{row}',valueInputOption='USER_ENTERED',body=Jpnl).execute()
-    row+=1
-    with open('row.json','w') as f:
-      json.dump({"row":row},f)
 
 def long(en,prices,rsi):
     global client
@@ -94,4 +52,3 @@ def close(ex,pnl,prices,rsi,type):
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
             smtp.login(Email_Adress, Email_Password)
             smtp.send_message(msg)
-    record(prices,rsi,"close",pnl)
