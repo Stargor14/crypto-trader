2085acbd4054ed45ab6a6815ad504b0472f7e45b
Eryk Halicki <stargor14@outlook.com>
2021-02-28 18:12:41 -0500

    commit

---
+++ req.py
@@ -0,0 +1,10 @@
+import json
+from binance.client import Client
+
+with open('keys.json','r') as r:
+    data  = json.load(r)
+    api_key = data['public']
+    api_secret = data['secret']
+
+client = Client(api_key, api_secret)
+print(api_key)
